b4299599101e3e06f162b4d68a03ab781e3b0a76
404NotFound <3138947771@qq.com>
2025-08-27 11:43:27 +0800

    UARTTTY

--- uarttty_reg_func.c
+++ uarttty_reg_func.c
@@ -16,26 +16,31 @@
 extern struct tty_t tty;
 int32_t mem_show_line_cnt = 8;
 
-int64_t uarttty_help(uint64_t para1) {
+int64_t uarttty_help(uint64_t para1)
+{
 
-    UARTTTY_CVT_PARA_TO_CHAR_P(para1,str);
+    UARTTTY_CVT_PARA_TO_CHAR_P(para1, str);
 
-    if (str[0]=='\0') {
-        for (int i = 0; i < tty.func_num; i++) {
+    if (str[0] == '\0')
+    {
+        for (int i = 0; i < tty.func_num; i++)
+        {
             printf("func_name: '%s'\r\npara_num: %d\r\npara_type: ", tty.func_table[i].func_name, tty.func_table[i].para_num);
-            for (int j =0;j<tty.func_table[i].para_num;j++) {
-                switch (tty.func_table[i].para_type[j]) {
-                    case UARTTTY_TYPE_INTEGER:
-                        printf("%s ","Integer");
+            for (int j = 0; j < tty.func_table[i].para_num; j++)
+            {
+                switch (tty.func_table[i].para_type[j])
+                {
+                case UARTTTY_TYPE_INTEGER:
+                    printf("%s ", "Integer");
                     break;
-                    case UARTTTY_TYPE_STRING:
-                        printf("%s ","String");
+                case UARTTTY_TYPE_STRING:
+                    printf("%s ", "String");
                     break;
-                    case UARTTTY_TYPE_DOUBLE:
-                        printf("%s ","Double");
+                case UARTTTY_TYPE_DOUBLE:
+                    printf("%s ", "Double");
                     break;
-                    default:
-                        printf("%s ","Unknown");
+                default:
+                    printf("%s ", "Unknown");
                     break;
                 }
             }
@@ -44,14 +49,19 @@ int64_t uarttty_help(uint64_t para1) {
             putchar('\r');
             putchar('\n');
         }
-    }else {
-        for (int i = 0; i < tty.func_num; i++) {
-            if (uarttty_strcmp(str, tty.func_table[i].func_name) == 0) {
+    }
+    else
+    {
+        for (int i = 0; i < tty.func_num; i++)
+        {
+            if (uarttty_strcmp(str, tty.func_table[i].func_name) == 0)
+            {
                 printf("%s\r\n", tty.func_table[i].func_help);
                 break;
             }
-            if (i == tty.func_num - 1) {
-                printf("no function called '%s'\r\n",str);
+            if (i == tty.func_num - 1)
+            {
+                printf("no function called '%s'\r\n", str);
                 printf("get function list by '?(\"\")'\r\n");
             }
         }
@@ -59,47 +69,61 @@ int64_t uarttty_help(uint64_t para1) {
     return 0;
 }
 
-int64_t show_uarttty_cfg() {
+int64_t show_uarttty_cfg()
+{
     printf("MemTools Cfg:\r\n");
-    printf("  mem_show_line_cnt=%d\r\n",mem_show_line_cnt);
+    printf("  mem_show_line_cnt=%d\r\n", mem_show_line_cnt);
     return 0;
 }
 
-int64_t uarttty_cfg(uint64_t para1,uint64_t para2) {
-    UARTTTY_CVT_PARA_TO_CHAR_P(para1,para);
-    UARTTTY_CVT_PARA_TO_INT32(para2,num);
+int64_t uarttty_cfg(uint64_t para1, uint64_t para2)
+{
+    UARTTTY_CVT_PARA_TO_CHAR_P(para1, para);
+    UARTTTY_CVT_PARA_TO_INT32(para2, num);
 
-    if (uarttty_strcmp(para,"mem_show_line_cnt") == 0) {
+    if (uarttty_strcmp(para, "mem_show_line_cnt") == 0)
+    {
         mem_show_line_cnt = num;
-        printf("set mem_show_line_cnt=%d\r\n",mem_show_line_cnt);
+        printf("set mem_show_line_cnt=%d\r\n", mem_show_line_cnt);
     }
     return 0;
 }
 
-int64_t read_byte(uint64_t para1,uint64_t para2,uint64_t para3) {
+int64_t read_byte(uint64_t para1, uint64_t para2, uint64_t para3)
+{
 
-    UARTTTY_CVT_PARA_TO_UINT32(para1,address);
-    UARTTTY_CVT_PARA_TO_UINT16(para2,length);
-    UARTTTY_CVT_PARA_TO_UINT8(para3,bit_mode);
+    UARTTTY_CVT_PARA_TO_UINT32(para1, address);
+    UARTTTY_CVT_PARA_TO_UINT16(para2, length);
+    UARTTTY_CVT_PARA_TO_UINT8(para3, bit_mode);
 
     uint8_t inline_num = 0;
-    printf("Memory Table from %10p to %10p", (uint8_t* )address, (uint8_t* )address + length - 1);
-    for (uint32_t addr = address; addr < address + length; addr++) {
-        const uint8_t data = *(uint8_t*)addr;
-        if (bit_mode == 0) {
-            if (inline_num == 0) {
-                printf("\r\n%10p : %02x", (uint8_t* )addr, (unsigned int)data);
-            }else {
+    printf("Memory Table from %10p to %10p", (uint8_t *)address, (uint8_t *)address + length - 1);
+    for (uint32_t addr = address; addr < address + length; addr++)
+    {
+        const uint8_t data = *(uint8_t *)addr;
+        if (bit_mode == 0)
+        {
+            if (inline_num == 0)
+            {
+                printf("\r\n%10p : %02x", (uint8_t *)addr, (unsigned int)data);
+            }
+            else
+            {
                 printf(" %02x", (unsigned int)data);
             }
-        }else {
-            if (inline_num == 0) {
-                printf("\r\n%10p : ", (uint8_t* )addr);
-                for (uint8_t mask = 0x80 ; mask != 0; mask >>= 1)
+        }
+        else
+        {
+            if (inline_num == 0)
+            {
+                printf("\r\n%10p : ", (uint8_t *)addr);
+                for (uint8_t mask = 0x80; mask != 0; mask >>= 1)
                     putchar(data & mask ? '1' : '0');
                 putchar(' ');
-            }else {
-                for (uint8_t mask = 0x80 ; mask != 0; mask >>= 1)
+            }
+            else
+            {
+                for (uint8_t mask = 0x80; mask != 0; mask >>= 1)
                     putchar(data & mask ? '1' : '0');
                 putchar(' ');
             }
@@ -112,83 +136,87 @@ int64_t read_byte(uint64_t para1,uint64_t para2,uint64_t para3) {
     return 0;
 }
 
-int64_t write_byte(uint64_t para1,uint64_t para2) {
+int64_t write_byte(uint64_t para1, uint64_t para2)
+{
 
-    UARTTTY_CVT_PARA_TO_UINT32(para1,address);
-    UARTTTY_CVT_PARA_TO_UINT8(para2,data);
+    UARTTTY_CVT_PARA_TO_UINT32(para1, address);
+    UARTTTY_CVT_PARA_TO_UINT8(para2, data);
 
-    *(uint8_t*)address = data;
+    *(uint8_t *)address = data;
     volatile uint8_t rd_data = 0;
-    rd_data = *(uint8_t*)address;
-    printf("1st read %10p : %02x\r\n", (uint8_t* )address, (unsigned int)rd_data);
-    rd_data = *(uint8_t*)address;
-    printf("2nd read %10p : %02x\r\n", (uint8_t* )address, (unsigned int)rd_data);
+    rd_data = *(uint8_t *)address;
+    printf("1st read %10p : %02x\r\n", (uint8_t *)address, (unsigned int)rd_data);
+    rd_data = *(uint8_t *)address;
+    printf("2nd read %10p : %02x\r\n", (uint8_t *)address, (unsigned int)rd_data);
     return 0;
 }
 
-int64_t add_demo(uint64_t para1,uint64_t para2, uint64_t para3,uint64_t para4) {
+int64_t add_demo(uint64_t para1, uint64_t para2, uint64_t para3, uint64_t para4)
+{
 
-    UARTTTY_CVT_PARA_TO_INT32(para1,n1);
-    UARTTTY_CVT_PARA_TO_INT32(para2,n2);
-    UARTTTY_CVT_PARA_TO_DOUBLE(para3,d1);
-    UARTTTY_CVT_PARA_TO_DOUBLE(para4,d2);
+    UARTTTY_CVT_PARA_TO_INT32(para1, n1);
+    UARTTTY_CVT_PARA_TO_INT32(para2, n2);
+    UARTTTY_CVT_PARA_TO_DOUBLE(para3, d1);
+    UARTTTY_CVT_PARA_TO_DOUBLE(para4, d2);
 
-    printf("%lf+%lf=%lf\r\n", d1, d2, d1+d2);
+    printf("%lf+%lf=%lf\r\n", d1, d2, d1 + d2);
 
-    return n1+n2;
+    return n1 + n2;
 }
 
-int64_t printf_demo(uint64_t para1,uint64_t para2,uint64_t para3) {
+int64_t printf_demo(uint64_t para1, uint64_t para2, uint64_t para3)
+{
 
-    UARTTTY_CVT_PARA_TO_CHAR_P(para1,fmt_str);
-    UARTTTY_CVT_PARA_TO_INT32(para2,n);
-    UARTTTY_CVT_PARA_TO_DOUBLE(para3,d);
+    UARTTTY_CVT_PARA_TO_CHAR_P(para1, fmt_str);
+    UARTTTY_CVT_PARA_TO_INT32(para2, n);
+    UARTTTY_CVT_PARA_TO_DOUBLE(para3, d);
 
-    printf(fmt_str,n,d);
+    printf(fmt_str, n, d);
     return 0;
 }
 
-void UARTTTY_func_reg() {
-    UARTTTY_register(uarttty_help,"?",1,"S",
-        "function '?' is for function list or function list\r\n"
-        "use '?(\"\")' to get all registered function\r\n"
-        "use '?(\"func_name\") to get target function help\r\n'");
+void UARTTTY_func_reg()
+{
+    UARTTTY_register(uarttty_help, "?", 1, "S",
+                     "function '?' is for function list or function list\r\n"
+                     "use '?(\"\")' to get all registered function\r\n"
+                     "use '?(\"func_name\") to get target function help\r\n'");
 
-    UARTTTY_register(show_uarttty_cfg,"show_uarttty_cfg",0,"",
-        "function 'show_uarttty_cfg' shows the self defined config\r\n"
-        "all the config type is int32_t\r\n");
+    UARTTTY_register(show_uarttty_cfg, "show_uarttty_cfg", 0, "",
+                     "function 'show_uarttty_cfg' shows the self defined config\r\n"
+                     "all the config type is int32_t\r\n");
 
-    UARTTTY_register(uarttty_cfg,"uarttty_cfg",2,"SN",
-        "function 'uarttty_cfg' can edit the self defined config\r\n"
-        "para1(string) is the config name\r\n"
-        "para2(int32_t) is the config value\r\n");
+    UARTTTY_register(uarttty_cfg, "uarttty_cfg", 2, "SN",
+                     "function 'uarttty_cfg' can edit the self defined config\r\n"
+                     "para1(string) is the config name\r\n"
+                     "para2(int32_t) is the config value\r\n");
 
-    UARTTTY_register(read_byte,"read_byte",3,"NNN",
-         "function 'read_byte' can read bytes and display value to the serialport\r\n"
-         "para1(uint32_t) is the begin memory address\r\n"
-         "para2(uint16_t) is the length to read\r\n"
-         "para3(uint8_t) is bit_mode, input 0 or 1\r\n"
-         "e.g. read_byte(0x00000100,32,0)");
+    UARTTTY_register(read_byte, "read_byte", 3, "NNN",
+                     "function 'read_byte' can read bytes and display value to the serialport\r\n"
+                     "para1(uint32_t) is the begin memory address\r\n"
+                     "para2(uint16_t) is the length to read\r\n"
+                     "para3(uint8_t) is bit_mode, input 0 or 1\r\n"
+                     "e.g. read_byte(0x00000100,32,0)");
 
-    UARTTTY_register(write_byte,"write_byte",2,"NN",
-        "function 'write_byte' can write bytes to memory\r\n"
-        "para1(uint32_t) is the target memory address\r\n"
-        "para2(uint8_t) is the value\r\n");
+    UARTTTY_register(write_byte, "write_byte", 2, "NN",
+                     "function 'write_byte' can write bytes to memory\r\n"
+                     "para1(uint32_t) is the target memory address\r\n"
+                     "para2(uint8_t) is the value\r\n");
 
-    UARTTTY_register(printf_demo,"printf_demo",3,"SND",
-        "function 'printf_demo' is a para simple test\r\n"
-        "para1(string) is the printf format\r\n"
-        "para2(int32_t) is a number\r\n"
-        "para3(double) is a number\r\n"
-        "in function will call the printf(para1,para2,para3)\r\n"
-        "e.g. printf_demo(\"recv value: %d %lf\\r\\n\",42,-42.5)\r\n");
+    UARTTTY_register(printf_demo, "printf_demo", 3, "SND",
+                     "function 'printf_demo' is a para simple test\r\n"
+                     "para1(string) is the printf format\r\n"
+                     "para2(int32_t) is a number\r\n"
+                     "para3(double) is a number\r\n"
+                     "in function will call the printf(para1,para2,para3)\r\n"
+                     "e.g. printf_demo(\"recv value: %d %lf\\r\\n\",42,-42.5)\r\n");
 
-    UARTTTY_register(add_demo,"add_demo",4,"NNDD",
-    "function 'add_demo' is a para simple test\r\n"
-    "para1(int32_t) is first integer\r\n"
-    "para2(int32_t) is second integer\r\n"
-    "para3(double) is first double\r\n"
-    "para4(double) is second double\r\n"
-    "e.g. add_demo(-15, 0x15, -12.5, 30.25)\r\n");
+    UARTTTY_register(add_demo, "add_demo", 4, "NNDD",
+                     "function 'add_demo' is a para simple test\r\n"
+                     "para1(int32_t) is first integer\r\n"
+                     "para2(int32_t) is second integer\r\n"
+                     "para3(double) is first double\r\n"
+                     "para4(double) is second double\r\n"
+                     "e.g. add_demo(-15, 0x15, -12.5, 30.25)\r\n");
 }
 #endif
--- uarttty_str.c
+++ uarttty_str.c
@@ -8,13 +8,17 @@
 
 #include "uarttty_str.h"
 
-int32_t uarttty_str_find_index(const char* str,const int32_t start_index,const char ch) {
-    if (start_index < 0) {
+int32_t uarttty_str_find_index(const char *str, const int32_t start_index, const char ch)
+{
+    if (start_index < 0)
+    {
         return -1;
     }
     int32_t index = start_index;
-    while (str[index] != '\0') {
-        if (ch == str[index]) {
+    while (str[index] != '\0')
+    {
+        if (ch == str[index])
+        {
             return index;
         }
         index++;
@@ -22,26 +26,29 @@ int32_t uarttty_str_find_index(const char* str,const int32_t start_index,const c
     return -1;
 }
 
-int32_t uarttty_is_blank(const char ch) {
-    switch (ch) {
-        case ' ':
-        case '\n':
-        case '\r':
-        case '\t':
-        case '\v':
-        case '\f':
-            return 1;
-        default:
-            return 0;
+int32_t uarttty_is_blank(const char ch)
+{
+    switch (ch)
+    {
+    case ' ':
+    case '\n':
+    case '\r':
+    case '\t':
+    case '\v':
+    case '\f':
+        return 1;
+    default:
+        return 0;
     }
 }
 int64_t uarttty_str_to_int(const char *str)
 {
-    int32_t num = 0;
+    int64_t num = 0;
     int8_t negflag = 1;
     int16_t start_index = 0;
     uint8_t hexmode = 0;
-    while (uarttty_is_blank(str[start_index])) {
+    while (uarttty_is_blank(str[start_index]))
+    {
         start_index++;
     }
     if (str[start_index] == '-')
@@ -54,19 +61,19 @@ int64_t uarttty_str_to_int(const char *str)
         negflag = 1;
         start_index = 1;
     }
-    else if (str[start_index+1] == 'x' || str[start_index+1] == 'X') {
+    else if (str[start_index + 1] == 'x' || str[start_index + 1] == 'X')
+    {
         negflag = 1;
         start_index += 2;
         hexmode = 1;
     }
 
-    for (int i = start_index; str[i] <= '9' && str[i] >= '0'
-        || str[i] <= 'F' && str[i] >= 'A'
-        || str[i] <= 'f' && str[i] >= 'a'; i++)
+    for (int i = start_index; str[i] <= '9' && str[i] >= '0' || str[i] <= 'F' && str[i] >= 'A' || str[i] <= 'f' && str[i] >= 'a'; i++)
     {
         if (hexmode == 0)
             num = num * 10 + (str[i] - '0');
-        else {
+        else
+        {
             if (str[i] <= 'f' && str[i] >= 'a')
                 num = num * 16 + (str[i] - 'a' + 10);
             else if (str[i] <= 'F' && str[i] >= 'A')
@@ -80,32 +87,43 @@ int64_t uarttty_str_to_int(const char *str)
     return num * negflag;
 }
 
-double uarttty_str_to_double(const char * str) {
+double uarttty_str_to_double(const char *str)
+{
     double num = 0;
     double divnum = 1;
     int8_t negflag = 1;
     int8_t pointflag = 0;
     int16_t start_index = 0;
-    while (uarttty_is_blank(str[start_index])) {
+    while (uarttty_is_blank(str[start_index]))
+    {
         start_index++;
     }
-    if (str[start_index] == '-') {
+    if (str[start_index] == '-')
+    {
         negflag = -1;
         start_index += 1;
     }
-    else if (str[start_index] == '+') {
+    else if (str[start_index] == '+')
+    {
         negflag = 1;
         start_index += 1;
     }
 
-    for (int i = start_index ;(str[i] <= '9' && str[i] >= '0') || str[i] == '.'; i++) {
-        if (str[i] == '.') {
-            if (pointflag == 0) {
+    for (int i = start_index; (str[i] <= '9' && str[i] >= '0') || str[i] == '.'; i++)
+    {
+        if (str[i] == '.')
+        {
+            if (pointflag == 0)
+            {
                 pointflag = 1;
-            }else {
+            }
+            else
+            {
                 return 0;
             }
-        }else {
+        }
+        else
+        {
             num = num * 10 + (str[i] - '0');
             if (pointflag == 1)
                 divnum = divnum * 10;
@@ -114,35 +132,43 @@ double uarttty_str_to_double(const char * str) {
     return num * negflag / divnum;
 }
 
-void uarttty_strcpy(const char* src, char* dest) {
-    while (1){
-		*dest = *src;
-		if(*dest == '\0')
-			break;
-		dest++;
-		src++;
-	};
+void uarttty_strcpy(const char *src, char *dest)
+{
+    while (1)
+    {
+        *dest = *src;
+        if (*dest == '\0')
+            break;
+        dest++;
+        src++;
+    };
 }
 
 void uarttty_strncpy(const char *src, char *dest, uint32_t size)
 {
-    do{
-        if (size == 0){
+    do
+    {
+        if (size == 0)
+        {
             *dest = '\0';
             return;
         }
-		*dest = *src;
-		if(*dest == '\0'){
-			break;
-		}
-		dest++;
-		src++;
-    }while(size--);
+        *dest = *src;
+        if (*dest == '\0')
+        {
+            break;
+        }
+        dest++;
+        src++;
+    } while (size--);
 }
-int32_t uarttty_strcmp(const char* cmp1,const char* cmp2) {
+int32_t uarttty_strcmp(const char *cmp1, const char *cmp2)
+{
     int32_t index = 0;
-    while (cmp1[index] == cmp2[index]) {
-        if (cmp1[index] == '\0') {
+    while (cmp1[index] == cmp2[index])
+    {
+        if (cmp1[index] == '\0')
+        {
             return 0;
         }
         index++;
